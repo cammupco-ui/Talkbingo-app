706ed94dd0d712539db6a0497f29eb46f312430b
TalkBingo Deploy <anna@cammuplabs.com>
2026-06-09 22:09:59 -0400

    fix: replace custom glove painter with 👇 emoji pointer

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4ebd1e3e3cb89b1ab7d04a4bc7253e12",
+const RESOURCES = {"flutter_bootstrap.js": "46af7b43506ef3ec03b7fa40261aa156",
 "guide_bingo.html": "4590b6fd44b5a1f538a00ba4339cb50f",
 "version.json": "66e60995956dc62c016ca47644e635f0",
 "index.html": "59fba95b2a25d43e6d3b11aa00a1e943",
 "/": "59fba95b2a25d43e6d3b11aa00a1e943",
 "CNAME": "6a833849f699a4a5a3da962b67295abb",
 "googleadd3a33693be7912.html": "f2873ca2d490a615aef1f828b4bfa919",
-"main.dart.js": "8c588286c218fde5167ce71e37d915d5",
+"main.dart.js": "4da5b2fe2b1dc01841dfbf8d83895ef9",
 "privacy_en.html": "08c85510f8f610dd19c375026a085285",
 "ads.txt": "f3d4401d0fbf94bf64c519aaeb384a27",
 ".well-known/apple-app-site-association": "92207d5a27fc0f17be8ad46d1d6823d3",
